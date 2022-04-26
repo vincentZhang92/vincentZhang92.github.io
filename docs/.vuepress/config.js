e28d9802d0680a29d1f2f399c7445f5b7ffb9415
Vincent Zhang <vincentzhang92@163.com>
2022-04-26 10:14:39 +0800

    调整最后更新日期格式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,15 @@
+const moment = require('moment');
+
 module.exports = {
   title: 'VincentZhang的笔记',
   description: 'Just playing around',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'VincentZhang的笔记',
+      description: 'Just playing around'
+    }
+  },
   shouldPrefetch: () => false,
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
@@ -23,11 +32,21 @@ module.exports = {
         }
       ]
     },
-    lastUpdated: '最后更新日期：',
+    lastUpdated: '最后更新日期',
     repo: 'vincentZhang92/vincentZhang92.github.io',
     docsDir: 'docs',
     docsBranch: 'docs',
     editLinks: true,
     editLinkText: '帮助VincentZhang92改善此页面！'
-  }
+  },
+  plugins: [[
+    '@vuepress/last-updated',
+    {
+      transformer: (timestamp, lang) => {
+        const moment = require('moment');
+        moment.locale(lang);
+        return moment(timestamp).fromNow();
+      }
+    }
+  ]]
 }
